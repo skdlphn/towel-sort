0edfa6235d4d543eafb996de19d69a0c3153cf08
sergey <skazantsev@lineate.com>
2020-01-21 21:31:43 +0600

    towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+module.exports = function towelSort (matrix = []) {
+    return matrix.length === 0 ? [] : matrix.reduce((prevVal, item, index) => {
+      if(index % 2 !== 0){
+          item = item.reverse();
+      }
+      prevVal.push(...item);
+      return prevVal;
+  }, []);
+};
